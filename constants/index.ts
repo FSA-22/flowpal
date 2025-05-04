32a648136d30c654954bdb7f3824477c98678c23
FSA-22 <simeonstat@gmail.com>
2025-05-04 22:56:08 +0000

    change text

--- constants/index.ts
+++ constants/index.ts
@@ -65,28 +65,28 @@ export const testimonials: Testimonial[] = [
   {
     id: 3,
     name: 'Aisha M.',
-    text: 'The profit and expenses insights have helped me optimize my pricinf strategy. My revenue has increased by 30% in just three months!',
+    text: 'The profit and expenses insights have helped me optimize my pricing strategy. My revenue has increased by 30% in just three months!',
     avatarUrl: '/aisha-m.jpg',
     position: 'E-commerce Enterpreneur',
   },
   {
     id: 4,
     name: 'Aisha M.',
-    text: 'The profit and expenses insights have helped me optimize my pricinf strategy. My revenue has increased by 30% in just three months!',
+    text: 'The profit and expenses insights have helped me optimize my pricing strategy. My revenue has increased by 30% in just three months!',
     avatarUrl: '/aisha-m.jpg',
     position: 'E-commerce Enterpreneur',
   },
   {
     id: 5,
     name: 'Aisha M.',
-    text: 'The profit and expenses insights have helped me optimize my pricinf strategy. My revenue has increased by 30% in just three months!',
+    text: 'The profit and expenses insights have helped me optimize my pricing strategy. My revenue has increased by 30% in just three months!',
     avatarUrl: '/aisha-m.jpg',
     position: 'E-commerce Enterpreneur',
   },
   {
     id: 6,
     name: 'Aisha M.',
-    text: 'The profit and expenses insights have helped me optimize my pricinf strategy. My revenue has increased by 30% in just three months!',
+    text: 'The profit and expenses insights have helped me optimize my pricing strategy. My revenue has increased by 30% in just three months!',
     avatarUrl: '/aisha-m.jpg',
     position: 'E-commerce Enterpreneur',
   },
@@ -113,7 +113,7 @@ export const questions = [
   },
   {
     id: '4',
-    question: 'How can I get support if i have issues?',
+    question: 'How can I get support if I have issues?',
     answer:
       'You can reach out to our support team through the contact form on our website or via email. We are here to assist you with any issues you may encounter.',
   },
